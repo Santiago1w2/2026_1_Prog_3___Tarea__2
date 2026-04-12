c441ce798f9b24aee7ef3ed3fed017a7252a5f78
Limepal <gabrieldiazcarrion07@gmail.com>
2026-04-12 12:21:52 -0500

    Funcion max y transform reduce

--- main.cpp
+++ main.cpp
@@ -5,21 +5,21 @@
 using namespace std;
 template < typename C >
 concept Iterable = requires ( C c ) {
-    std::begin ( c ) ;
-    std::end ( c ) ;
+    begin ( c ) ;
+    end ( c ) ;
 };
 template < typename T >
 concept Addable = requires ( T a , T b ) {
-    { a + b } -> std::same_as <T >;
+    { a + b } -> same_as <T >;
 };
 template < typename T >
 concept Divisible = requires ( T a , std::size_t n ) {
-    { a / n } -> std::same_as <T>;
+    { a / n } -> same_as <T>;
 };
 
 template <typename T>
 concept Multipliable = requires (T a, T b) {
-    { a * b } -> std::same_as<T>;
+    { a * b } -> same_as<T>;
 };
 namespace core_numeric {
     template <typename T>
@@ -47,18 +47,57 @@ namespace core_numeric {
         }
         return mean(desviacion_cuadrado);
     }
+    template <typename T>
+    requires Iterable<T>
+        T max(vector<T>& data) {
+        T max = data[0];
+        for (auto x : data) {
+            if (x > max) {
+                max = x;
+            }
+        }
+        return max;
+    }
+    template <typename c, typename f>
+    auto transform_reduce(const c& v, f transform) {
+        using T = decltype(transform(*v.begin()));
+
+        T result = 0;
+
+        for (const auto& element : v) {
+            result += transform(element);
+        }
+
+        return result;
+    }
 
 
 }
+
 
 int main() {
     /*vector<string> v{"2", "2", "3"};
     auto m = core_numeric::sum(v);
     cout << m << endl;*/
-
+    /*
     vector<double> v{9, 8,10,11,12};
     auto m = core_numeric::variance(v);
     cout << m << endl;
+    */
 
+    //Prueba 2
+    /*
+    vector<string> data = {"a", "b", "c"};
+
+    cout << core_numeric::max(data) << endl;
+    */
+    std::vector<double> v = {1.0, 2.0, 3.0, 4.0};
+
+    // Usage as seen in your image:
+    auto r = core_numeric::transform_reduce(v, [](double x) {
+        return x * x; // The "multiployable" logic
+    });
+
+    std::cout << r << std::endl; // Output: 30
     return 0;
 }
